11a1b3c85b82254170c69d2a934531d008cfdfa2
taskcrypto <56127239+taskcrypto@users.noreply.github.com>
2025-03-18 23:29:54 +0900

    ver1.1.4

--- lib/builder.dart
+++ lib/builder.dart
@@ -2,7 +2,5 @@ import 'package:build/build.dart';
 
 import 'src/builder/openapi_builder.dart';
 
-export 'src/builder/openapi_builder.dart';
-
 /// OpenAPIジェネレーターのビルダー
 Builder openapiBuilder(BuilderOptions options) => OpenApiBuilder();
